e1c9001c518215284af728f753789de8e5b4c35d
su-tran75 <su-tran@hotmail.fr>
2021-10-13 17:49:12 +0800

    create featuredInfo.jsx + add style

---
+++ react-admin-dashboard/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.scss";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+    </div>
+  );
+}
