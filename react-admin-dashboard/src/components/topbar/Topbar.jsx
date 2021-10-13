79ef42d0e89003feb2074ba4fbe14b5a47191bb1
su-tran75 <su-tran@hotmail.fr>
2021-10-13 11:43:03 +0800

    create topbar.jsx + add style

---
+++ react-admin-dashboard/src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./topbar.scss";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Dashboard</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/364308/pexels-photo-364308.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            alt="avatar image"
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
