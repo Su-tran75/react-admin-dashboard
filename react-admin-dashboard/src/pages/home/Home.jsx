74f5caeefd1a5e0c37f2ec62648aa5c7372919ba
su-tran75 <su-tran@hotmail.fr>
2021-10-13 18:32:49 +0800

    create Chart.jsx using Rechart lib, add style and set dynamic info with DummyData.js

--- react-admin-dashboard/src/pages/home/Home.jsx
+++ react-admin-dashboard/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./home.scss";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+import { userData } from "../../DummyData";
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
     </div>
   );
 }
